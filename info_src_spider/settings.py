2d93e7ac6430b581f8b6f0ffe5a18d4ba8d90327
ZoomCypher <30547004+ZoomCypher@users.noreply.github.com>
2018-03-24 10:09:55 +0800

    Add files via upload

---
+++ info_src_spider/settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# IP
+HOST='192.168.2.104'
+# myslq用户名
+USER='dev'
+# Mysql密码
+PASSWORD='dev'
+# myslq数据库
+DB='msite_test'
+# myslq端口
+PORT=3306
