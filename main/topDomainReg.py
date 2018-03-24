ffc4b4c6d09c7d000398bcdb9085adbe3c002d5f
ZoomCypher <30547004+ZoomCypher@users.noreply.github.com>
2018-03-24 11:00:24 +0800

    Add files via upload

---
+++ main/topDomainReg.py
@@ -0,0 +1,28 @@
+topHostPostfix = [
+    '.com','.la','.io','.co','.info','.net','.org','.me','.mobi',
+    '.us','.biz','.xxx','.ca','.co.jp','.com.cn','.net.cn',
+    '.org.cn','.mx','.tv','.ws','.ag','.com.ag','.net.ag',
+    '.org.ag','.am','.asia','.at','.be','.com.br','.net.br',
+    '.bz','.com.bz','.net.bz','.cc','.com.co','.net.co',
+    '.nom.co','.de','.es','.com.es','.nom.es','.org.es',
+    '.eu','.fm','.fr','.gs','.in','.co.in','.firm.in','.gen.in',
+    '.ind.in','.net.in','.org.in','.it','.jobs','.jp','.ms',
+    '.com.mx','.nl','.nu','.co.nz','.net.nz','.org.nz',
+    '.se','.tc','.tk','.tw','.com.tw','.idv.tw','.org.tw',
+    '.hk','.co.uk','.me.uk','.org.uk','.vg', ".com.hk" ]
+
+
+doubleTopDomain = [ '.co.jp','.com.cn','.net.cn','.co.uk',
+                    '.org.cn''.com.br','.net.br',
+                    '.com.bz','.net.bz','.com.co',
+                    '.net.co','.nom.co','.com.mx',
+                    '.com.es','.nom.es','.org.es',
+                    '.co.in','.firm.in','.gen.in',
+                    '.ind.in','.net.in','.org.in',
+                    '.co.nz','.net.nz','.org.nz',
+                    '.com.tw','.idv.tw','.org.tw',
+                    '.me.uk','.org.uk', ".com.hk"
+                    ]
+
+
+RegexForJudgeDomain = '[com|la|io|co|info|net|org|me|mobi|us|biz|xxx|ca|co.jp|com.cn|net.cn|org.cn|mx|tv|ws|ag|com.ag|net.ag|org.ag|am|asia|at|be|com.br|net.br|bz|com.bz|net.bz|cc|com.co|net.co|nom.co|de|es|com.es|nom.es|org.es|eu|fm|fr|gs|in|co.in|firm.in|gen.in|ind.in|net.in|org.in|it|jobs|jp|ms|com.mx|nl|nu|co.nz|net.nz|org.nz|se|tc|tk|tw|com.tw|idv.tw|org.tw|hk|co.uk|me.uk|org.uk|vg|com.hk]'
